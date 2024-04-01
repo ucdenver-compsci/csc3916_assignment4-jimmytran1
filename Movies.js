26c1901652ac159505920e57376aeeafbb6bf663
jimmytran1 <jimmytran935@gmail.com>
2024-03-31 21:02:09 -0600

    Added review route

--- Movies.js
+++ Movies.js
@@ -4,9 +4,22 @@ var Schema = mongoose.Schema;
 mongoose.connect(process.env.DB);
 
 // Movie schema
-var MovieSchema = new Schema({
+const MovieSchema = new mongoose.Schema({
+    title: { type: String, required: true, index: true },
+    releaseDate: Date,
+    genre: {
+      type: String,
+      enum: [
+        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      ],
+    },
+    actors: [{
+      actorName: String,
+      characterName: String,
+    }],
+  });
 
-});
+  const Movie = mongoose.model('Movie', MovieSchema);
 
 // return the model
 module.exports = mongoose.model('Movie', MovieSchema);
